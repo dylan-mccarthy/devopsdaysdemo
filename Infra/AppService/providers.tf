a2d28f866e10e34556e522b1efbd44a42b1d4c6d
Dylan McCarthy <dylan.mccarthy@azenix.com.au>
2023-03-13 04:58:59 +0000

    updates

--- Infra/AppService/providers.tf
+++ Infra/AppService/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.7.0"
     }
   }
 
@@ -10,9 +10,11 @@ terraform {
     resource_group_name  = "rg-dod-demo-tfstate"
     storage_account_name = "doddemotfstatestorage"
     container_name       = "tfstate"
+    use_oidc = true
   }
 }
 
 provider "azurerm" {
     features {}
+    use_oidc = true
 }
